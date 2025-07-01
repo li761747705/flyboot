eee6555711bc7aad3cce6e383bb037195fd4e541
lilinjin0520 <761747705@qq.com>
2025-07-01 15:40:20 +0800

    修复问题

--- src/views/About.vue
+++ src/views/About.vue
@@ -71,7 +71,7 @@
                 </div>
               </div>
             </div>
-            
+      
             <div class="col-md-4 mb-4">
               <div class="about-card">
                 <div class="about-image">
